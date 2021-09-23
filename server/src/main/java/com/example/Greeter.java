01bedaeec0396c2e7be25f1273e7ba95b89f1e40
sambhaji <sambhaji@gmail.com>
2021-09-23 15:20:11 +0530

    Revert "Update Greeter.java"
    This reverts commit 48d4779cae02508a6738bc55a013332f2787acf8.

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,18 +1,19 @@
-import java.util.Scanner;
+package com.example;
 
-public class HelloWorld {
+/**
+ * This is a class.
+ */
+public class Greeter {
 
-    public static void main(String[] args) {
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
 
-        // Creates a reader instance which takes
-        // input from standard input - keyboard
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+  }
 
-        // nextInt() reads the next integer from the keyboard
-        int number = reader.nextInt();
-
-        // println() prints the following line to the output screen
-        System.out.println("You entered: " + number);
-    }
+  //TODO: Add javadoc comment
+  public String greet(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
 }
